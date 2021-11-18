f69823041c8e7de1f917f26d3f72abded2cf8138
Juyoung Lee <ejuyoung@kaist.ac.kr>
2021-11-18 16:29:46 +0900

    add error img display add information retrieval

--- flask-server/main.py
+++ flask-server/main.py
@@ -5,11 +5,12 @@ Created on Wed Nov 17 21:28:50 2021
 @author: JY
 """
 from flask import Flask
-from flask import render_template, send_file
+from flask import jsonify, render_template, send_file, redirect
 
 import os
 import pandas as pd
 
+from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__,
                  static_folder='static',
@@ -22,13 +23,37 @@ DB = pd.read_csv("artworks_db.csv", index_col=0)
 def img_list():
     return render_template('list.html')
 
-
-@app.route('/id/<int:report_id>', methods=['GET', 'POST'])
+@app.route('/info/id/<int:report_id>', methods=['GET', 'POST'])
+def get_info_by_id(report_id):
+    try:
+        return DB.loc[report_id].to_json(orient = 'records', force_ascii=False)
+    except Exception as err:
+        print(repr(err))
+        return jsonify(error=repr(err))
+    
+@app.route('/img/id/<int:report_id>', methods=['GET', 'POST'])
 def get_img_by_id(report_id):
-    if report_id in DB.index:
-        return send_file(DB.img_path[report_id])
-    else:
-        return send_file(os.path.join("img","ctar.png"))
+    try:
+        img_path = DB.img_path[report_id]
+        if img_path[:4] == "http":
+            return redirect(img_path)
+        else:
+            return send_file(img_path)
+    except Exception as err:
+        print(repr(err))
+        return send_file(create_text_to_image(repr(err)))
 
+def create_text_to_image(str_):
+    img = Image.new('RGB', (500,500), color = (73, 109, 137))
+ 
+    fnt = ImageFont.truetype(os.path.join('static','arial.ttf'), 30)
+    d = ImageDraw.Draw(img)
+    d.text((10,10), str_, font=fnt, fill=(255, 255, 0))
+     
+    tmp_path = os.path.join("img","error.png")
+    img.save(tmp_path)
+    
+    return tmp_path
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=12572, debug=True)
--- unity-client/GalleryExample/Assets/Scripts/GetDisplayIMG.cs
+++ unity-client/GalleryExample/Assets/Scripts/GetDisplayIMG.cs
@@ -7,11 +7,23 @@ using UnityEngine.Networking;
 public class GetDisplayIMG : MonoBehaviour
 {
     public GameObject rawImage;
-    public string img_url_pre = "http://localhost:12572/id/";
+    public string info_url_pre = "http://localhost:12572/info/id/";
+    public string img_url_pre = "http://localhost:12572/img/id/";
 
+    IEnumerator  DlInfo(string addr){   
+        UnityWebRequest request = UnityWebRequest.Get(addr);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError ||
+            request.result == UnityWebRequest.Result.ProtocolError){
+                Debug.LogError(request.error);
+        }
+        else{
+            Debug.Log("DlInfo:: retrieved: "+request.downloadHandler.text);
+        }
+            
+    }
     IEnumerator  DlImage(object[] parms){   
-        Debug.Log("DownloadImage: call");
-
         RawImage img = (RawImage)parms[0];
         UnityWebRequest request = UnityWebRequestTexture.GetTexture((string)parms[1]);
         yield return request.SendWebRequest();
@@ -21,9 +33,7 @@ public class GetDisplayIMG : MonoBehaviour
                 Debug.LogError(request.error);
         }
         else{
-
             img.texture = ((DownloadHandlerTexture) request.downloadHandler).texture;
-            Debug.Log("DownloadImage: texture set");
         }
             
     }
@@ -42,22 +52,26 @@ public class GetDisplayIMG : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha0)) {
             object[] parms = new object[2]{rawImage.GetComponent<RawImage> (), img_url_pre+"0"};
+            StartCoroutine("DlInfo", info_url_pre+"0");
             StartCoroutine("DlImage", parms);
 
         }
         else if (Input.GetKeyDown(KeyCode.Alpha1)) {
             object[] parms = new object[2]{rawImage.GetComponent<RawImage> (), img_url_pre+"1"};
             StartCoroutine("DlImage", parms);
+            StartCoroutine("DlInfo", info_url_pre+"1");
 
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2)) {
             object[] parms = new object[2]{rawImage.GetComponent<RawImage> (), img_url_pre+"2"};
             StartCoroutine("DlImage", parms);
+            StartCoroutine("DlInfo", info_url_pre+"2");
 
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3)) {
             object[] parms = new object[2]{rawImage.GetComponent<RawImage> (), img_url_pre+"3"};
             StartCoroutine("DlImage", parms);
+            StartCoroutine("DlInfo", info_url_pre+"3");
 
         }
     }
